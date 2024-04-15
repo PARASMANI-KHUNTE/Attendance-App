dd6035f7880cc7a74ee9d83031d0bc3900751df4
PARASMANIKHUNTE <137080546+PARASMANI-KHUNTE@users.noreply.github.com>
2024-04-15 10:53:57 +0530

    Update app.js
    avtar

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const names = [
-    { name: "ABHISHEK PAUL", rollNo: 1, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
-    { name: "ADITYA KUMAR SINGH", rollNo: 2, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
+    { name: "ABHISHEK PAUL", rollNo: 1, imgSrc: "https://i.pinimg.com/564x/7a/9c/c3/7a9cc36f478969da92e2e9fee391a5b2.jpg" },
+    { name: "ADITYA KUMAR SINGH", rollNo: 2, imgSrc: "https://i.pinimg.com/736x/50/10/86/5010861721f41329deeefef7a1c048f5.jpg" },
     { name: "AKASH SHARMA", rollNo: 3, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "ANJALI", rollNo: 4, imgSrc: "https://imgs.search.brave.com/GrvFTEK-P2lH1DL2WE6ubJgILzv4FV-Kypi9jEZQ9ZU/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvY3V0ZS13b21h/bi1hdmF0YXItcHJv/ZmlsZS12ZWN0b3It/aWxsdXN0cmF0aW9u/XzEwNTg1MzItMTQ1/OTIuanBnP3NpemU9/NjI2JmV4dD1qcGc" },
     { name: "ANSH TRIPATHI", rollNo: 5, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "ARNOLD MINJ", rollNo: 6, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "ARVIND SONI", rollNo: 7, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
-    { name: "AYUSH RAI", rollNo: 8, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
-    { name: "B. VAMSI", rollNo: 9, imgSrc: "https://i.pinimg.com/736x/b8/1e/55/b81e55b2571a7266083cc8bf4c7c0dd2.jpg" },
+    { name: "AYUSH RAI", rollNo: 8, imgSrc: "https://i.pinimg.com/564x/cb/5a/06/cb5a0695c231b1e532748daeae3e6601.jpg" },
+    { name: "B. VAMSI", rollNo: 9, imgSrc: "https://i.pinimg.com/564x/9c/12/56/9c1256b353bfa55bc219b534ad997cca.jpg" },
     { name: "CHITESH KUMAR SAHU", rollNo: 11, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "HARSHVARDHAN KUMAR", rollNo: 12, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "HEENA KUMARI SAHU", rollNo: 13, imgSrc: "https://i.pinimg.com/564x/c1/d6/5e/c1d65eb71bfb042058570252b8fa522e.jpg" },
@@ -32,7 +32,7 @@ const names = [
     { name: "RISHABH JOGI", rollNo: 32, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "RISHABH VAISHNAV", rollNo: 33, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "RIYA SINGH", rollNo: 34, imgSrc: "https://i.pinimg.com/564x/3b/19/fc/3b19fc0712d259dfafcfc2c140b8d04b.jpg" },
-    { name: "SAGAR SINGH", rollNo: 35, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
+    { name: "SAGAR SINGH", rollNo: 35, imgSrc: "https://i.pinimg.com/originals/7c/12/fa/7c12fae69e5388431ea18af2a07df3b5.gif" },
     { name: "SAKSHAM SHRIVASTAVA", rollNo: 36, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "SARTHAK BHAGAT", rollNo: 37, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "SATYANSH KAHARA", rollNo: 38, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
@@ -47,7 +47,7 @@ const names = [
     { name: "SUSHIL PATEL", rollNo: 47, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "TEJ PRATAP SHARMA", rollNo: 48, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
     { name: "TRIPURARI NATH", rollNo: 49, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
-    { name: "VAIBHAV KHARE", rollNo: 50, imgSrc: "https://imgs.search.brave.com/t_EO3diQ0lNqEI0BznGqzktsXdr8cd-_ABxoymHugIc/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4x/Lmljb25maW5kZXIu/Y29tL2RhdGEvaWNv/bnMvdXNlci1waWN0/dXJlcy8xMDAvbWFs/ZTMtNTEyLnBuZw" },
+    { name: "VAIBHAV KHARE", rollNo: 50, imgSrc: "https://i.pinimg.com/564x/4d/2b/e1/4d2be140612cf109167172f82bb3597c.jpg" },
     { name: "VARUN KAIWART", rollNo: 51, imgSrc: "https://i.pinimg.com/564x/d9/21/60/d92160da86a546289978a4d589e434bf.jpg" }
   ];
   
